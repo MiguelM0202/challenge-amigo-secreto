9bcf0f6909882aed7fcefc2f8ee6be1aeae71bc2
MiguelM0202 <miguel.montero0202@gmail.com>
2025-03-16 12:08:56 -0500

    Actualización proyecto 16/03. Se validaron varios métodos para crear una lista efectiva donde se creara una etiqueta <li> por cada ingreso del usuario. Después de varios intentos y consultas de la MDN se opta por crear una etiqueta <li> con el id=listaAmigos1 y el resultado es el esperado. Al integrar una función de loop con for donde el método de salida se da al llegar al máximo de intentos permitidos y se deshabilita el botón Añadir. Se genera una condicional donde al agregar la información del usuario solo si no esta repetida permite el ingreso a la etiqueta padre <li> de lo contrario crea otra etiqueta <li> hija. Se espera continuar avanzado. El siguiente paso es generar el sorteo y condiciones iniciales para reiniciar el juego con el boton Añadir.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 let amigoSorteado = 0;
-//let amigosIngresados = 0;//
+let amigosIngresados = 0;
 let listaDeAmigosIngresados = [];
-let numeroMaximoDeAmigos = 5;
-
+let numeroMaximoDeAmigos = 3;
 
 /*funcion asignar texto*/
-function asignarTextoElemento(elemento,texto) {
-    let elementoHTML = document.getElementById(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
+function asignarTextoElemento(elemnto,texto) {
+    let elementoHTML = document.querySelector(elemnto);
+    for (let i = 0; i < numeroMaximoDeAmigos; i++) {
+       if (listaDeAmigosIngresados.length === numeroMaximoDeAmigos+1){
+            alert("Ingresaste el numero maximo de amigos");
+            document.querySelector("#Añadir").setAttribute("disabled", "true");
+            break;
+       } else{
+        if (listaDeAmigosIngresados.includes(texto)) {
+            let nuevoElemnto = document.createElement("li");
+            document.getElementById("listaAmigos1").appendChild(nuevoElemnto);
+            nuevoElemnto.innerHTML = texto;
+            return elementoHTML;
+        } else{
+            elementoHTML.innerHTML = texto;
+            return;
+        }
+       }}}
+
 
 /*Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".*/
 function agregarAmigo() {
@@ -18,39 +31,42 @@ function agregarAmigo() {
     console.log(ingresoUsuario);
     console.log(listaDeAmigosIngresados);
 /*Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.*/
-    if (ingresoUsuario == ("")) {
+    if (ingresoUsuario == ("")||ingresoUsuario >= parseInt(0/1)) {
         alert ("Por favor, inserte un nombre.");
-    } else {
-        //Si el nombre ingresado está incluido en la lista 
+        limpiarCaja();
+    }else{
+    //Si el nombre ingresado está incluido en la lista 
         if (listaDeAmigosIngresados.includes(ingresoUsuario)) {
-            asignarTextoElemento("amigoRepetido","Nombre ya ingresado, ingresa otro o intenta con el segundo nombre de tu amigo c:");
-            return;
-            //Si el nombre ingresado no está incluido en la lista 
-        } else {
-            listaDeAmigosIngresados.push(ingresoUsuario);
-            /*Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.*/
-            listaDeAmigosIngresados.forEach(ingresoUsuario => {
-                let listado = document.getElementById("listaAmigos");
-                let nombreAmigo = (document.createElement("li"));
-                nombreAmigo.textContent = ingresoUsuario;
-                listado.appendChild(nombreAmigo);
-            });
+            alert ("Ya ingresaste ese nombre. Intenta con otro por favor c:");
             limpiarCaja();
             return;
+        }else{
+    //Si el nombre ingresado no está incluido en la lista 
+            listaDeAmigosIngresados.push(ingresoUsuario);
+    //Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+            console.log (ingresoUsuario);
+            asignarTextoElemento("li",`${ingresoUsuario}`);
+
+            limpiarCaja();
+            amigosIngresados++;
+            return;
         }
-    }
+      }
 }
+    
 
 
-
+            
 /*limpiar caja*/
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
-    asignarTextoElemento("amigoRepetido", "");
     document.querySelector("#amigo").focus();
 }
 
-/*Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.*/
+//Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.*/
+/*let listaLista = Math.floor(Math.random()*numeroMaximoDeAmigos)+1;
+    
+console.log(listaLista);*/
 
 //Validar que haya amigos disponibles: Antes de sortear, comprobar si el array amigos no está vacío.
 
